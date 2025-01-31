71f91776279a8ea42f09674c11d146aaff9c54f7
Jordan Tsagmo <jordantsagmo@gmail.com>
2025-01-31 21:19:26 +0100

    mise en place du domain

---
+++ Bank-System/src/test/java/karibu/formation/com/banksystem/BankSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package karibu.formation.com.banksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
